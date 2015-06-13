368b0513988e1cf4e83665940bc1c06c402b1dd4
Alfonso Gocht <agocht@gmail.com>
2015-06-13 18:01:23 -0500

    fix

--- teamsalud/apps/location/models.py
+++ teamsalud/apps/location/models.py
@@ -3,25 +3,25 @@ from django.db import models
 # Create your models here.
 
 class TipoCategoria(models.Model):
-    codigo=models.CharField(
+    codigo = models.CharField(
         max_length=10
     )
 
 
 class Condicion(models.Model):
-    codigo=models.CharField(
+    codigo = models.CharField(
         max_length=50
     )
-    nombre=models.CharField(
+    nombre = models.CharField(
         max_length=300
     )
 
 
 class SignoAlerta(models.Model):
-    codigo=models.CharField(
+    codigo = models.CharField(
         max_length=50
     )
-    descripcion=models.CharField(
+    descripcion = models.CharField(
         max_length=300
     )
 
@@ -32,14 +32,15 @@ class CondicionSignoAlerta(models.Model):
         'Condicion',
         related_name='condicion_set',
         )
-    signo_alerta=models.ForeignKey(
+    signo_alerta = models.ForeignKey(
         'SignoAlerta',
         related_name='signo_alerta_set'
     )
-    tipo_categoria=models.ForeignKey(
+    tipo_categoria = models.ForeignKey(
         'TipoCategoria',
         related_name='tipo_categoria_set'
     )
+    extra = models.CharField(max_length=100)
 
 
 __all__ = [
