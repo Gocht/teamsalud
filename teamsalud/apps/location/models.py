844f572bf1d66c14c0afb7185b165c12f11ab5e7
gescah <gescah@gmail.com>
2015-06-13 17:22:16 -0500

    models

--- teamsalud/apps/location/models.py
+++ teamsalud/apps/location/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TipoCategoria(models.Model):
+    codigo=models.CharField(
+        max_length=10
+    )
+
+    class Meta:
+        app_label = 'loc'
+
+
+class Condicion(models.Model):
+    codigo=models.CharField(
+        max_length=50
+    )
+    nombre=models.CharField(
+        max_length=300
+    )
+
+    class Meta:
+        app_label = 'loc'
+
+
+class SignoAlerta(models.Model):
+    codigo=models.CharField(
+        max_length=50
+    )
+    descripcion=models.CharField(
+        max_length=300
+    )
+
+    class Meta:
+        app_label = 'loc'
+
+
+class CondicionSignoAlerta(models.Model):
+
+    condicion = models.ForeignKey(
+        'Condicion',
+        related_name='condicion_set',
+        )
+    signo_alerta=models.ForeignKey(
+        'SignoAlerta',
+        related_name='signo_alerta_set'
+    )
+    tipo_categoria=models.ForeignKey(
+        'TipoCategoria',
+        related_name='tipo_categoria_set'
+    )
+
+    class Meta:
+        app_label = 'loc'
+
+
