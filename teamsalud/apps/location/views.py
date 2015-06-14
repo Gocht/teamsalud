47726aacdf28d45b1b191eeb4f00b68286d7faab
Alfonso Gocht <agocht@gmail.com>
2015-06-13 22:11:32 -0500

    fix

--- teamsalud/apps/location/views.py
+++ teamsalud/apps/location/views.py
@@ -5,7 +5,7 @@ from models import Condicion
 
 
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'test.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
