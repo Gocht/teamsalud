e0ab8c21ebc2aad26347189ee273a0ee1e07b05b
gescah <gescah@gmail.com>
2015-06-13 22:56:50 -0500

    sajbvhjd

--- teamsalud/apps/location/views.py
+++ teamsalud/apps/location/views.py
@@ -5,7 +5,7 @@ from models import Condicion, CondicionSignoAlerta, SignoAlerta, TipoCategoria, 
 
 
 class HomeView(TemplateView):
-    template_name = 'test.html'
+    template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
