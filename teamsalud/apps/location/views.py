1d9d0ae3a3415817fd9845a305befd1f396064d9
Alfonso Gocht <agocht@gmail.com>
2015-06-13 20:11:37 -0500

    fix

--- teamsalud/apps/location/views.py
+++ teamsalud/apps/location/views.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-	template_name = 'base.html'
+    template_name = 'home.html'
 
 
--- teamsalud/teamsalud/settings.py
+++ teamsalud/teamsalud/settings.py
@@ -74,7 +74,7 @@ TIME_ZONE = 'America/Lima'
 USE_I18N = True
 
 USE_L10N = True
-
+    
 USE_TZ = True
 
 
